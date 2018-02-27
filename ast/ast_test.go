af519a79cfd67c369b3d73ce835917657d061299
mewmew <rnd0x00@gmail.com>
2018-02-27 13:08:32 +0100

    ast: add test cases

---
+++ ast/ast_test.go
@@ -0,0 +1,96 @@
+package ast_test
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+
+	"github.com/mewkiz/pkg/osutil"
+	"github.com/mewmew/l/ast"
+	"github.com/mewmew/l/lexer"
+	"github.com/mewmew/l/parser"
+	"github.com/pkg/errors"
+)
+
+func TestParse(t *testing.T) {
+	golden := []struct {
+		path string
+	}{
+		{path: "testdata/cat.ll"},
+	}
+	for _, g := range golden {
+		buf, err := ioutil.ReadFile(g.path)
+		if err != nil {
+			t.Errorf("%q: unable to read file %q; %v", g.path, g.path, err)
+			continue
+		}
+		want := string(buf)
+		if wantPath := g.path + ".golden"; osutil.Exists(wantPath) {
+			buf, err := ioutil.ReadFile(wantPath)
+			if err != nil {
+				t.Errorf("%q: unable to read file %q; %v", g.path, wantPath, err)
+				continue
+			}
+			want = string(buf)
+		}
+		l := lexer.NewLexer(buf)
+		p := parser.NewParser()
+		module, err := p.Parse(l)
+		if err != nil {
+			t.Errorf("%q: unable to parse file %q; %v", g.path, g.path, err)
+			continue
+		}
+		m, ok := module.(*ast.Module)
+		if !ok {
+			t.Errorf("%q: invalid module type, expected *ast.Module, got %T", g.path, module)
+			continue
+		}
+		got := m.String()
+		if want != got {
+			if err := diff(want, got); err != nil {
+				log.Fatalf("%q: unable to diff `%v` and `%v`; %v", g.path, want, got, err)
+			}
+			t.Errorf("%q: module mismatch", g.path)
+			//t.Errorf("%q: module mismatch; expected `%v`, got `%v`", g.path, want, got)
+			continue
+		}
+	}
+}
+
+func diff(a, b string) error {
+	dir, err := ioutil.TempDir("/tmp", "diff_")
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	path := filepath.Join(dir, "foo")
+	if err := ioutil.WriteFile(path, []byte(a), 0644); err != nil {
+		return errors.WithStack(err)
+	}
+	cmd := exec.Command("git", "init")
+	cmd.Dir = dir
+	if err := cmd.Run(); err != nil {
+		return errors.WithStack(err)
+	}
+	cmd = exec.Command("git", "add", path)
+	cmd.Dir = dir
+	if err := cmd.Run(); err != nil {
+		return errors.WithStack(err)
+	}
+	if err := ioutil.WriteFile(path, []byte(b), 0644); err != nil {
+		return errors.WithStack(err)
+	}
+	cmd = exec.Command("git", "diff", "--color-words")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Dir = dir
+	if err := cmd.Run(); err != nil {
+		return errors.WithStack(err)
+	}
+	if err := os.RemoveAll(dir); err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
