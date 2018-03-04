6fa612bf13458d15d5d7d69f76523096c7e186dd
mewmew <rnd0x00@gmail.com>
2018-03-04 23:35:12 +0100

    asm: add package to parse LLVM IR modules
    Returns *ast.Module for now, will be updated in the
    future to return *ir.Module instead.

---
+++ asm/asm.go
@@ -0,0 +1,69 @@
+// Package asm implements a parser for LLVM IR assembly.
+package asm
+
+import (
+	"io"
+	"io/ioutil"
+
+	"github.com/mewmew/l/asm/internal/ast"
+	"github.com/mewmew/l/asm/internal/lexer"
+	"github.com/mewmew/l/asm/internal/parser"
+	"github.com/pkg/errors"
+)
+
+// ParseFile parses the given LLVM IR assembly file into an LLVM IR module.
+func ParseFile(path string) (*ast.Module, error) {
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return ParseBytes(buf)
+}
+
+// Parse parses the given LLVM IR assembly file into an LLVM IR module, reading
+// from r.
+func Parse(r io.Reader) (*ast.Module, error) {
+	buf, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return ParseBytes(buf)
+}
+
+// ParseBytes parses the given LLVM IR assembly file into an LLVM IR module,
+// reading from b.
+func ParseBytes(b []byte) (*ast.Module, error) {
+	module, err := parseBytes(b)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return module, nil
+	// Translate the AST of the module to an equivalent LLVM IR module.
+	//m, err := irx.Translate(module)
+	//if err != nil {
+	//	return nil, errors.WithStack(err)
+	//}
+	//return m, nil
+}
+
+// ParseString parses the given LLVM IR assembly file into an LLVM IR module,
+// reading from s.
+func ParseString(s string) (*ast.Module, error) {
+	return ParseBytes([]byte(s))
+}
+
+// parseBytes parses the given LLVM IR assembly file into an AST, reading from
+// b.
+func parseBytes(b []byte) (*ast.Module, error) {
+	l := lexer.NewLexer(b)
+	p := parser.NewParser()
+	module, err := p.Parse(l)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	m, ok := module.(*ast.Module)
+	if !ok {
+		return nil, errors.Errorf("invalid module type; expected *ast.Module, got %T", module)
+	}
+	return m, nil
+}
