8b0ef3011eed3bc10bfffd41d2c5ee73b16dccc6
mewmew <rnd0x00@gmail.com>
2018-03-10 01:46:01 +0100

    irx: translate top-level entities and add cat.ll test case

--- asm/internal/irx/module.go
+++ asm/internal/irx/module.go
@@ -35,6 +35,18 @@ func (m *Module) indexIdents(entities []ast.TopLevelEntity) {
 	// Create maps from identifier to definition.
 	for _, entity := range entities {
 		switch entity := entity.(type) {
+		case *ir.SourceFilename:
+			// Source filename.
+			m.SourceFilename = entity
+		case *ir.TargetTriple:
+			// Target triple.
+			m.TargetTriple = entity
+		case *ir.DataLayout:
+			// Data layout.
+			m.DataLayout = entity
+		case *ir.ModuleAsm:
+			// Module-level inline assembly.
+			m.ModuleAsms = append(m.ModuleAsms, entity)
 		case *types.NamedType:
 			// Type definitions.
 			ident := entity.Name
@@ -57,13 +69,14 @@ func (m *Module) indexIdents(entities []ast.TopLevelEntity) {
 			}
 			m.globalIdent[ident] = entity
 		case *ir.IndirectSymbol:
-			// Indirect symbol definitions.
+			// Indirect symbol definitions (aliases and IFuncs).
 			ident := entity.Name
 			if prev, ok := m.globalIdent[ident]; ok {
 				panic(fmt.Errorf("global identifier %q already present; prev `%v`, new `%v`", ident, prev, entity))
 			}
 			m.globalIdent[ident] = entity
 		case *ir.Function:
+			// Function declarations and definitions.
 			ident := entity.Name
 			if prev, ok := m.globalIdent[ident]; ok {
 				panic(fmt.Errorf("global identifier %q already present; prev `%v`, new `%v`", ident, prev, entity))
@@ -90,6 +103,12 @@ func (m *Module) indexIdents(entities []ast.TopLevelEntity) {
 				panic(fmt.Errorf("metadata ID %q already present; prev `%v`, new `%v`", id, prev, entity))
 			}
 			m.metadataID[id] = entity
+		case *ir.UseListOrder:
+			// Use-list order directives.
+			m.UseListOrders = append(m.UseListOrders, entity)
+		case *ir.UseListOrderBB:
+			// Basic block specific use-list order directives.
+			m.UseListOrderBBs = append(m.UseListOrderBBs, entity)
 		default:
 			panic(fmt.Errorf("support for top-level entity %T not yet implemented", entity))
 		}
